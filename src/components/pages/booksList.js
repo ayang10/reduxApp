35ac3ee1b288c374e6e9a6c03e4d01be795bacd8
ayang10 <ayang014@gmail.com>
2017-11-02 20:48:32 -0400

    thursday night commit

--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getBooks } from '../../actions/booksActions';
+import { Grid, Col, Row, Button } from 'react-bootstrap';
+
+import BookItem from './bookItem';
 
 class BooksList extends Component {
     componentDidMount() {
@@ -12,18 +15,22 @@ class BooksList extends Component {
     render() {
         const booksList = this.props.books.map(function(booksArr) {
             return (
-                <div key={booksArr.id}>
-                    <h2>{booksArr.title}</h2>
-                    <h2>{booksArr.description}</h2>
-                    <h2>{booksArr.price}</h2>
-                </div>
+               <Col xs={12} sm={6} md={4} key={booksArr.id}>
+                <BookItem 
+                    id={booksArr.id}
+                    title={booksArr.title}
+                    description={booksArr.description}
+                    price={booksArr.price}
+                />
+               </Col>
             )
         })
         return (
-            <div>
-                <h1>Hello React</h1>
-                {booksList}
-            </div>
+            <Grid>
+                <Row style={{marginTop: '15px'}}>
+                    {booksList}
+                </Row>
+            </Grid>
         )
     }
 }
