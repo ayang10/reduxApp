888b3cf54d02c04e968c2a98c34432745b7dbf2c
ayang10 <ayang014@gmail.com>
2017-11-11 10:37:47 -0500

    saturday morning commit
    setting up mongoose and doing post request

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+// REACT-ROUTER
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
@@ -14,15 +16,30 @@ import reducers from './reducers/index';
 import { addToCart } from './actions/cartActions';   
 import { postBooks, deleteBooks, updateBooks } from './actions/booksActions';
 
+
 // Step 1 create the store
 const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
 
 import BooksList from './components/pages/booksList';
+import Cart from './components/pages/cart';
+import BooksForm from './components/pages/booksForm';
+import Main from './main';
+
+
+const Routes = (
+    <Provider store={store}>
+            <Router history={browserHistory}>
+                <Route path="/" component={Main}>
+                    <IndexRoute component={BooksList} />
+                    <Route path="/admin" component={BooksForm} />
+                    <Route path="/cart" component={Cart} />
+                </Route>
+            </Router>
+    </Provider>
+)
 
 render(
-    <Provider store={store}>
-        <BooksList />
-    </Provider>, document.getElementById('app')
+    Routes, document.getElementById('app')
 )
 
